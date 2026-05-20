3de35990d7b956491dab437f98cd38eca580d5ac
anshu <anshhufx@gmail.com>
2026-05-20 23:21:24 +0530

    add zoom out effect

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,28 +3,30 @@ import { IconPlus } from "@tabler/icons-react";
 const Card = () => {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
-      <div className="h-95 w-70 bg-white rounded-3xl flex flex-col justify-between">
+      <div className="h-95 w-70 bg-white rounded-3xl flex flex-col justify-between cursor-pointer">
         <div className=" flex justify-center items-center p-3">
           <div className="relative rounded-2xl overflow-hidden">
-            {/* main img */}
-            <img
-              className="h-72 w-65 object-cover"
-              src="https://plus.unsplash.com/premium_photo-1714195646981-221ce73e0d5f?q=80&w=689&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="img"
-            />
+            <div className="transition-transform duration-500 ease-out hover:scale-110">
+              {/* main img */}
+              <img
+                className="h-72 w-65 object-cover"
+                src="https://plus.unsplash.com/premium_photo-1714195646981-221ce73e0d5f?q=80&w=689&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="img"
+              />
 
-            {/* BLUR LAYER */}
-            <div
-              className="absolute bottom-0 left-0 w-full h-28"
-              style={{
-                backdropFilter: "blur(12px)",
-                WebkitBackdropFilter: "blur(12px)",
-                maskImage:
-                  "linear-gradient(to top, black 0%, black 40%, transparent 100%)",
-                WebkitMaskImage:
-                  "linear-gradient(to top, black 0%, black 40%, transparent 100%)",
-              }}
-            />
+              {/* BLUR LAYER */}
+              <div
+                className="absolute bottom-0 left-0 w-full h-28"
+                style={{
+                  backdropFilter: "blur(12px)",
+                  WebkitBackdropFilter: "blur(12px)",
+                  maskImage:
+                    "linear-gradient(to top, black 0%, black 40%, transparent 100%)",
+                  WebkitMaskImage:
+                    "linear-gradient(to top, black 0%, black 40%, transparent 100%)",
+                }}
+              />
+            </div>
           </div>
         </div>
         <div className="flex justify-between p-3 pb-12">
@@ -42,7 +44,7 @@ const Card = () => {
               <p className="font-normal text-[9px] text-neutral-400">12m ago</p>
             </div>
           </div>
-          <div className="flex justify-center items-center bg-neutral-800 text-neutral-200 text-[12px] px-2.5 rounded-md gap-0.5 ">
+          <div className="flex justify-center items-center bg-neutral-800 text-neutral-200 text-[12px] px-2.5 rounded-md gap-0.5 transition-all duration-200 active:scale-95">
             <div className="">
               <IconPlus stroke={2} size={12} />
             </div>
